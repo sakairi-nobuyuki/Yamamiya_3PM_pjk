799e790834025aebece3851321a63c47fd695a15
sakairi-nobuyuki <SAKAIRI.Nobuyuki@gmail.com>
2023-05-02 16:30:39 +0900

    feat: training script

---
+++ ml_components/ml_components/io/__init__.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+from ml_components.io.io_template import IOTemplate
+from ml_components.io.save_image_s3 import S3ImageIO
+from ml_components.io.config_s3 import S3ConfigIO
---
+++ ml_components/ml_components/train/__init__.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+from ml_components.train.template_trainer import TemplateTrainer
+from ml_components.train.vgg_like_classifier_trainer import VggLikeClassifierTrainer
