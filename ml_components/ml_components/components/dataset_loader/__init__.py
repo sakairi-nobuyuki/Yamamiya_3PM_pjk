47feb80cef823718e8d8a02340cbc8265596defa
sakairi-nobuyuki <SAKAIRI.Nobuyuki@gmail.com>
2024-07-15 01:52:12 +0900

    fix: train umap again

--- ml_components/ml_components/components/dataset_loader/__init__.py
+++ ml_components/ml_components/components/dataset_loader/__init__.py
@@ -1,5 +1,4 @@
 # codeing: utf-8
 
 from .template_dataset_loader import TemplateDatasetLoader
-from .kaggle_dataset_loader import KaggleDatasetLoader
 from .custom_dataset_loader import CustomDatasetLoader
